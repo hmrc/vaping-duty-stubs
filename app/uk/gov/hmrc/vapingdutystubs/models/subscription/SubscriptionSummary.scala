598dc09bc07d0dda65a6fd41f07ec667b336917f
ghousedev <49005870+ghousedev@users.noreply.github.com>
2026-02-16 08:59:21 +0000

    VPD-1359: Remove fields not used in subscription summary

--- app/uk/gov/hmrc/vapingdutystubs/models/subscription/SubscriptionSummary.scala
+++ app/uk/gov/hmrc/vapingdutystubs/models/subscription/SubscriptionSummary.scala
@@ -77,9 +77,7 @@ final case class SubscriptionSummaryResponse(
   addressLine1: Option[String],
   addressLine2: Option[String],
   addressLine3: Option[String],
-  addressLine4: Option[String],
   postcode: Option[String],
-  country: Option[String],
   approvalStatus: ApprovalStatus,
   insolvencyFlag: Boolean
 )
--- test/uk/gov/hmrc/vapingdutystubs/models/SubscriptionSummarySpec.scala
+++ test/uk/gov/hmrc/vapingdutystubs/models/SubscriptionSummarySpec.scala
@@ -49,6 +49,6 @@ class SubscriptionSummarySpec extends SpecBase {
   class SetUp {
     val now                     = Instant.now(clock)
     val subscriptionSummaryJson =
-      """{"success":{"processingDate":"2024-06-11T15:07:47.838Z","organisationName":"testAwNwaIL Ltd","paperlessPreference":"1","emailAddress":"john.doe@example.com","verifiedEmail":"1","bouncedEmail":"0","addressLine1":"Flat 123","addressLine2":"1 Example Road","addressLine4":"London","postcode":"AB1 2CD","country":"GB","approvalStatus":"01","insolvencyFlag":"0"}}"""
+      """{"success":{"processingDate":"2024-06-11T15:07:47.838Z","organisationName":"testAwNwaIL Ltd","paperlessPreference":"1","emailAddress":"john.doe@example.com","verifiedEmail":"1","bouncedEmail":"0","addressLine1":"Flat 123","addressLine2":"1 Example Road","postcode":"AB1 2CD","approvalStatus":"01","insolvencyFlag":"0"}}"""
   }
 }
