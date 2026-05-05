21ae941c3c7751a0784b4eddd4603c7186f6774c
Rob Gresham <49005870+ghousedev@users.noreply.github.com>
2026-05-05 14:07:56 +0100

    VPD-1489: View multiple returns (#9)
    * VPD-1489: Add initial obligations controller
    
    * VPD-1489: Rebase

---
+++ app/uk/gov/hmrc/vapingdutystubs/controllers/ObligationsController.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vapingdutystubs.controllers
+
+import play.api.Logging
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.vapingdutystubs.models.obligations.{ObligationDetails, ObligationItem, ObligationsResponse}
+
+import java.time.LocalDate
+import javax.inject.{Inject, Singleton}
+
+@Singleton()
+class ObligationsController @Inject()(
+                                       cc: ControllerComponents
+                                     ) extends BackendController(cc) with Logging {
+
+  def get(): Action[AnyContent] = Action {
+      Ok(Json.toJson(createMockObligationsResponse()))
+  }
+
+  private def createMockObligationsResponse(): ObligationsResponse = {
+    val currentDate = LocalDate.now()
+
+    ObligationsResponse(
+      obligation = Seq(
+        // Outstanding return - Due
+        ObligationItem(
+          identification = None,
+          obligationDetails = ObligationDetails(
+            openOrFulfilledStatus = "O",
+            iCFromDate = LocalDate.of(2027, 12, 1),
+            iCToDate = LocalDate.of(2027, 12, 31),
+            iCDateReceived = None,
+            iCDueDate = currentDate.plusDays(10),
+            periodKey = "27AL"
+          )
+        ),
+        // Outstanding return - Overdue
+        ObligationItem(
+          identification = None,
+          obligationDetails = ObligationDetails(
+            openOrFulfilledStatus = "O",
+            iCFromDate = LocalDate.of(2027, 11, 1),
+            iCToDate = LocalDate.of(2027, 11, 30),
+            iCDateReceived = None,
+            iCDueDate = currentDate.minusDays(5),
+            periodKey = "27AK"
+          )
+        ),
+        // Completed return
+        ObligationItem(
+          identification = None,
+          obligationDetails = ObligationDetails(
+            openOrFulfilledStatus = "F",
+            iCFromDate = LocalDate.of(2027, 10, 1),
+            iCToDate = LocalDate.of(2027, 10, 31),
+            iCDateReceived = Some(LocalDate.of(2027, 11, 15)),
+            iCDueDate = LocalDate.of(2027, 11, 30),
+            periodKey = "27AJ"
+          )
+        )
+      )
+    )
+  }
+}
