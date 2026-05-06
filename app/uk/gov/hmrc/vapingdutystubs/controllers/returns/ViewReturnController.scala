4b5ef1e53329a83bc925674e973113fa7b7721ac
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-05-06 16:53:44 +0100

    VPD-1528: add routes and controller

---
+++ app/uk/gov/hmrc/vapingdutystubs/controllers/returns/ViewReturnController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.vapingdutystubs.controllers.returns
+
+import play.api.Logging
+import play.api.http.HeaderNames
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.vapingdutystubs.config.Constants.Headers.*
+import uk.gov.hmrc.vapingdutystubs.models.returns.submit.{ReturnCreateResponse, ReturnSubmittedResponse}
+import uk.gov.hmrc.vapingdutystubs.utils.LogHeadersHelper.logHeaders
+
+import java.time.{Instant, LocalDate}
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import uk.gov.hmrc.vapingdutystubs.data.returns.ReturnsData
+
+class ViewReturnController @Inject()(
+                                        cc: ControllerComponents,
+                                      )(using ExecutionContext) extends BackendController(cc)
+  with Logging {
+
+  def viewReturn(vpdReference: String, periodKey: String): Action[AnyContent] = Action {
+    implicit request =>
+      Ok(
+        Json.toJson(ReturnsData(
+          vpdReference,
+          periodKey,
+          submissionId = "submissionId"
+        ))
+      )
+  }
+}
